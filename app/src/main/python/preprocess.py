b11a3c8a56f721db8a4849b253898e19dd17cbf5
LuposX <schuppsimon5@gmail.com>
2020-03-27 18:31:04 +0100

    Added Chaquo to better preprocess Images

---
+++ app/src/main/python/preprocess.py
@@ -0,0 +1,26 @@
+from PIL import Image
+from PIL import ImageEnhance
+import os
+
+# preprocess the image
+def save_image(datadir):
+    image = Image.open(datadir + "/photo.png").convert('L').point(lambda x: 0 if x<170 else 1)
+
+    image = image.resize((128, 128))
+
+    new_size = 28
+
+    width, height = image.size
+
+    left = (width - new_size)/2
+    top = (height - new_size)/2
+    right = (width + new_size)/2
+    bottom = (height + new_size)/2
+    image_crop = image.crop((left, top, right, bottom))
+
+    # enhance the image
+    # enhance = ImageEnhance.Sharpness(image_crop).enhance(2)
+    # enhance_contrast = ImageEnhance.Contrast(enhance).enhance(2)
+
+    # save the image
+    image_crop.convert('L').save(datadir + "/out.png")
